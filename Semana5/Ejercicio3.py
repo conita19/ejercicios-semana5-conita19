147619005d5800556e4a4a030b443049ffef11b7
conita19 <165439635+conita19@users.noreply.github.com>
2026-04-11 18:12:30 -0400

    Update Ejercicio3.py

--- Semana5/Ejercicio3.py
+++ Semana5/Ejercicio3.py
@@ -1,6 +1,8 @@
 '''Cajero automático: validación de retiro
 Un cajero permite retirar solo montos mayores a 0 y múltiplos de 10.
 Solicita el monto hasta que sea válido y luego muestra "Retiro exitoso".'''
+dinero=(int(input("cuanto dinero quieres retirar del cajero autonatico?"))
+
 
 
 

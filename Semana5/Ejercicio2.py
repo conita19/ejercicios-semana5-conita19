760fa070aa6521fd630b02bccea123d3ff5b1d69
conita19 <165439635+conita19@users.noreply.github.com>
2026-04-13 22:00:56 -0400

    Update Ejercicio2.py

--- Semana5/Ejercicio2.py
+++ Semana5/Ejercicio2.py
@@ -5,6 +5,13 @@ Un sistema de climatización clasifica:
 "Calurosa": más de 25
 Solicita la temperatura e indica la clasificación correspondiente.
 '''
+x=float(input("Dime una temperatura y te dire a que clasificacion de temperatura corresponde:"))
+if x<10:
+    print(x,"Es una temperatura fria")
+elif (10<=x<=25):
+    print(x,"Es una temperatura templada")
+else:
+    print(x,"Es una temperatura calurosa") 
 
 
 

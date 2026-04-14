4aa566fe0bfc2e7f8d4b5e69d4117c16973553ee
conita19 <165439635+conita19@users.noreply.github.com>
2026-04-13 22:47:59 -0400

    Update Ejercicio4.py

--- Semana5/Ejercicio4.py
+++ Semana5/Ejercicio4.py
@@ -4,6 +4,14 @@ Solicita repetidamente:
 1 si asistió
 0 para terminar
 Al final, muestra cuántos días asistió.'''
-
+asistencia=0
+dias=1
+while(dias!=0):
+    dias=int(input("Dime si hoy has asistido a clases. Pon 1 para asitencia y 0 para inasistencia):"))
+    if dias==1:
+        asistencia+=1
+    elif dias!=0:
+        asistencia*=0
+print(asistencia)
 
 
